cea2fb96b150a6934c18915ffd66832b00e9dc9d
Yaroslav Dziubenko <Yar.dziub2005@gmail.com>
2025-02-10 19:39:30 +0200

    10.02.2025

--- img generation/main.py
+++ img generation/main.py
@@ -4,7 +4,7 @@ from diffusers import FluxPipeline
 pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 
-prompt = "A cat holding a sign that says hello world"
+prompt = "A cat holding a sign that says hello"
 image = pipe(
     prompt,
     height=1024,
